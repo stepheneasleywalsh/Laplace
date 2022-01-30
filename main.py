df678a54a33dba4b8476cd18ce0eb86cd3d58e0b
stephen.easley-walsh <stephen.easley-walsh@studygroup.com>
2022-01-30 21:33:27 +0000

    Update

--- main.py
+++ main.py
@@ -3,10 +3,12 @@ import matplotlib.pyplot as plt
 
 
 def f(t):
+    # JUST CHANGE THE FUNCTION HERE
     return math.sin(t)
 
 
 def integrate(f):
+    # UGLY BUT QUICK INTEGRATION CODING
     x = 0
     n = 0
     sum = 0
@@ -20,23 +22,21 @@ def integrate(f):
 
 
 def laplaceTransform(f,s):
+    # GETS THE LAPLACE TRANSFORM AT S
     def g(t):
         return math.exp(-s*t)
     return integrate(g)
 
 
+# PLOT IT
 xpoints = []
 ypoints = []
-
-
 x = 0
 while x < 3:
     y = laplaceTransform(f,x)
     ypoints.append(y)
     xpoints.append(x)
     x += 0.1
-
-
 plt.plot(xpoints,ypoints)
 plt.grid()
 plt.axhline(c="r")
@@ -44,5 +44,5 @@ plt.axvline(c="r")
 plt.title("The Laplace Transform of the function f(t) = sin(t)")
 plt.show()
 
-
+# QUIT
 quit()
